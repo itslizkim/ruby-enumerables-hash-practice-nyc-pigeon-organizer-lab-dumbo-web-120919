d81e637ae76018efa42058c611855dd2e5c372d2
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-08 18:15:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if result[name] == nil
+          result[name] = {}
+          result [name][key] = []
+        end
+
+        if result[name][key] == nil
+          result[name][key] = []
+        end
+        result[name][key] << new_value.to_s
+      end
+    end
+  end
+  result      # write your code here!
 end
