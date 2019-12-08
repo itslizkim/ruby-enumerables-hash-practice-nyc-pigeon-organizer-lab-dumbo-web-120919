e5391becbe230e599ace45d877d6f090ade7f3b3
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-08 18:08:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
 
-        if !new_hash[name]
+        if new_hash[name] == nil
           new_hash[name] = {}
         end  # write your code here!
 end
